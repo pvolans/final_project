2ef2b16d0682e49690988617c71410fde7baf491
pvolans <yusufsyckn2205@gmail.com>
2025-04-05 21:01:38 +0300

    tempCodeRunnerFile gitignore

--- DAQ Dev2/Software/tempCodeRunnerFile.py
+++ DAQ Dev2/Software/tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-115200
+     elif hasattr(self, "gcode_file") and self.gcode_file:  # Keep previous file
+            self.qtWindow.label_Status.setText("Using previously imported GCODE file.")
+    
