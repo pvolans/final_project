6e98479b34517206247954472d70645298cfdb44
pvolans <yusufsyckn2205@gmail.com>
2025-05-16 22:34:18 +0300

    Mutex added for serial com

--- DAQ Dev2/Software/scripts/serial_device.py
+++ DAQ Dev2/Software/scripts/serial_device.py
@@ -1,6 +1,6 @@
 # serial_device.py
 import serial
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, QMutex, pyqtSignal
 import queue
 
 class serial_device(QThread):
@@ -15,10 +15,13 @@ class serial_device(QThread):
         self.queue = queue.Queue()
 
         self.data_received.connect(data_handler)
+        self.mutex_ser = QMutex()
 
     def run(self):
         try:
             self.ser = serial.Serial(self.port_name, self.baudrate, timeout=0.1)
+
+
         except serial.SerialException as e:
             print(f"[ERROR] Could not open {self.port_name}: {e}")
             return
@@ -27,23 +30,35 @@ class serial_device(QThread):
             try:
                 if self.ser.in_waiting:
                     data = self.ser.readline()
+
                     if data:
                         decoded = data.decode(errors='ignore').strip()
                         self.queue.put(decoded)
                         self.data_received.emit(decoded)
+
             except serial.SerialException as e:
                 print(f"[ERROR] SerialException: {e}")
                 break
 
     def send(self, msg):
-        if self.ser and self.ser.is_open:
-            if isinstance(msg, str):
-                msg = msg.encode()
-            self.ser.write(msg)
+            if self.ser and self.ser.is_open:
+                if isinstance(msg, str):
+                    msg = msg.encode()
+                    
+                self.mutex_ser.lock()
+                try:
+                    self.ser.write(msg)
+                finally:
+                    self.mutex_ser.unlock()
 
     def stop(self):
         self._running = False
         if self.ser and self.ser.is_open:
-            self.ser.close()
+            self.mutex_ser.lock()
+            try:
+                self.ser.close()
+            finally:
+                self.mutex_ser.unlock()
+
         self.quit()
         self.wait()
